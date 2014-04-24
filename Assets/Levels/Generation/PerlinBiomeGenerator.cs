b3b227c9be54367e00878b7ad03b282a700a7ecc
PMP1 <Phil.pratt@live.co.uk>
2014-04-24 08:49:15 +0100

    updated biome generator

--- Assets/Levels/Generation/PerlinBiomeGenerator.cs
+++ Assets/Levels/Generation/PerlinBiomeGenerator.cs
@@ -1,5 +1,18 @@
 using System;
 namespace AssemblyCSharp {
 
+  public class PerlinBiomeGenerator : IBiomeGenerator {
+  
 
+    public PerlinBiomeGenerator() {
+
+    }
+
+    public float getHumidityAt(int x, int z) {
+
+
+    }
+
+   
+  }
 }
